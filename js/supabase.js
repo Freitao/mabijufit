a5b05e72de2395cc607afc628e3acc1807920608
Felipe Freitas <75279888+Freitao@users.noreply.github.com>
2026-09-19 08:19:17 -0300

    Adicionar configuração do Supabase

---
+++ js/supabase.js
@@ -0,0 +1,16 @@
+// =========================================================
+// MABIJUFIT — CONEXÃO SUPABASE
+// =========================================================
+
+const SUPABASE_URL =
+    "https://mzpvdtoofxepnmuwlxxh.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_RZkLXmshXJ081eDOJfthnA_EIjRv5fR";
+
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
